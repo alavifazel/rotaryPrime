a15e9995b622af868f6fb4a995775dd8dfb1d174
Iman Alavi Fazel <im.alavifazel@gmail.com>
2018-08-31 09:47:34 -0400

    first comit

---
+++ README.md
@@ -0,0 +1,1 @@
+This program find all rotary prime numbers meaning that by changing their digits, also prime numbers will be produced (like 313) up to 1 million.  
---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& v)
+{
+    os << "[";
+    for (int i = 0; i < v.size(); ++i) {
+        os << v[i];
+        if (i != v.size() - 1)
+            os << ", ";
+    }
+    os << "]\n";
+    return os;
+}
+ 
+bool isPrime(int number);
+bool isRotaryPrime(int number);
+vector<int> getPermutations(int number);
+void permute(vector<string> &pVec, string a, int l, int r);
+
+int main(){
+  vector <int> rotaryPrimes;
+  for(int i = 0; i < 1000000; ++i){
+    if(isPrime(i)){
+      if(isRotaryPrime(i)){
+      	  rotaryPrimes.push_back(i);
+      }
+    }
+  }
+  cout << rotaryPrimes;
+}
+
+
+bool isPrime(int number) {
+  if(number < 2){
+    return false;
+  }
+  for(int i = 2; i < int(sqrt(number)) + 1; ++i){
+    if(number % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool isRotaryPrime(int number){
+  vector<int> permutatedNumbers = getPermutations(number);
+  for(size_t i = 0; i < permutatedNumbers.size(); ++i){
+    if(!isPrime(permutatedNumbers[i])){
+      return false;
+    }
+  }
+  return true;
+}
+
+void permute(vector<string> &pVec, string a, int l, int r){
+  if(l == r){
+    pVec.push_back(a);
+  } else {
+    for(int i = l; i <= r; ++i){
+      swap(a.at(l), a.at(i));
+      permute(pVec, a, l+1, r);
+      swap(a.at(l), a.at(i));
+    }
+  }
+}
+
+vector<int> getPermutations(int number){
+  string strNumber = to_string(number);
+  vector<string> v;
+  vector<int> vi;
+  permute(v, strNumber, 0, strNumber.size() - 1);
+  for(const auto &i : v){
+    vi.push_back(stoi(i));
+  }
+  return vi;
+}
